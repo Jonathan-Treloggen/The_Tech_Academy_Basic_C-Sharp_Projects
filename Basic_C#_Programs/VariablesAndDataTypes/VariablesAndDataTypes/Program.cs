ab77d7722ba174055f197348c3a3c8bbe90869b0
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-09 15:52:37 -0700

    Adding additional assignments

---
+++ Basic_C#_Programs/VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // THIS IS A SIMPLE PROGRAM
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourname);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //Console.WriteLine("Your favorite number is: " + favoriteNumber);
+            //Console.ReadLine();
+
+            // IF YOU WANTED TO DO A MATH OPERATION ON THE CODE ABOVE
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+
+            //bool isStudying = false;
+            //byte numberOfHoursStudying = 40;
+            //sbyte currentTemperture = -23;
+            //char questionMark = '\u2103';
+            //Console.WriteLine(questionMark);
+            //Console.ReadLine();
+
+            //decimal moneyInBank = 100.4m;
+            //double heightInCentimeters = 211.33425452;
+
+            //float secondsLeft = 2.64f;
+            //short tempetureOnMars = -341;
+
+            //int curentAge = 27;
+            //string yearsOld = curentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/myConsoleProject/myConsoleProject.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace namespacemyConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
