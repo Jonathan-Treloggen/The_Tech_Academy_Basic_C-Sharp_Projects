af3d97662cda278abb9c3fd55bc9f4e8a25e4bcc
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-09 16:01:57 -0700

    more additions

---
+++ Basic_C#_Programs/Writing and Running code in C sharp assignment/C Sharp W and R Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
