f793acf9b0b82a674bdc070497d8a1f9238855d7
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-11 12:11:29 -0700

    BooleanLogicAssignment folder added to Assignment folder

---
+++ Basic_C#_Programs/Assignments/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What you age?");
+            string age = Console.ReadLine();
+            byte personsAge = Convert.ToByte(age);
+
+
+            Console.WriteLine("Have you ever had a DUI? Please answer true or false");
+            string DUI = Console.ReadLine();
+            bool anyDUIs = Convert.ToBoolean(DUI);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string tickets = Console.ReadLine();
+            byte numberOfTickets = Convert.ToByte(tickets);
+
+            Console.WriteLine("\nAre you Qualified?");
+            bool qualified = (personsAge > 15 && anyDUIs == false && numberOfTickets <= 3);
+            Console.WriteLine(qualified);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Notes and practice on C#/BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AND
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            // OR
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            // EQUALS
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            // NO EQUAL
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            // XOR OPERATOR IF ONE IS TRUE = TRUE BUT NOT BOTH
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
