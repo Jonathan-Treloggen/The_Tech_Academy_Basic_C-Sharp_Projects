f02f5cf8e5ac16c0c7f175f8f949304e84424361
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-15 15:14:53 -0700

    BooleanWhileAndDoWhileLoop folder added to Assignment folder and loop folder added to C-Sharp Challenges.

---
+++ Basic_C#_Programs/Assignments/BooleanWhileAndDowhileLoop/BooleanWhileAndDowhileLoop/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanWhileAndDowhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DO WHILE LOOP
+            Console.WriteLine("Whats my first name? go on, give it a guess.");
+            string guessedName = Console.ReadLine();
+
+            bool name = guessedName == "Jon";
+
+            do
+            {
+                switch (guessedName)
+                {
+                    case "Jon":
+                        Console.WriteLine("\nWow, you got it right, nice job.");
+                        name = true;
+                        break;
+
+                    case "John":
+                        Console.WriteLine("\nReally a silent h... no sir, close though.");
+                        Console.WriteLine("\nLet's try this again. Whats my first name?");
+                        guessedName = Console.ReadLine();
+                        break;
+
+                    case "Bob":
+                        Console.WriteLine("\nNo I'm not a Bob.");
+                        Console.WriteLine("\nLet's try this again. Whats my first name?");
+                        guessedName = Console.ReadLine();
+                        break;
+
+                    default:
+                        Console.WriteLine("\nOne hint, my name is 3-5 letters long.");
+                        Console.WriteLine("\nLet's try this again. Whats my first name?");
+                        guessedName = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!name);
+            Console.ReadLine();
+
+
+            // WHILE LOOP
+            Console.WriteLine("Time to count to ten! Press enter to continue.");
+            Console.ReadLine();
+            int n = 0;
+
+            while (n < 11)
+            {
+                Console.WriteLine(n);
+                if (n == 4)
+                {
+                    Console.WriteLine("Half way there!");
+                }
+                n++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Basic_C#_Programs/C-Sharp_Challange/C-Sharp_Challange/SwitchStatement/SwitchStatement/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day of the week is it? Please enter numbers only.");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch(day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Not even an answer, really?");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
