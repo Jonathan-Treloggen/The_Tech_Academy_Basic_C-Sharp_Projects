f052386668088376d0f2393413af11f960e00d20
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-16 11:51:18 -0700

    ArrayAndList folder added to Assignment folder

---
+++ Basic_C#_Programs/Assignments/ArrayAndLists/ArrayAndLists/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            // ARRAY WITH A STRING
+            Console.WriteLine("Pick a number from 0-3, this will be an idex in an array.");
+
+            string[] stringNum = { "3", "89", "33", "92" };
+            int Snum = Convert.ToInt32(Console.ReadLine());
+
+            if (Snum <= 3)
+            {
+                Console.WriteLine(stringNum[Snum]);
+            }
+            else
+            {
+                Console.WriteLine("\nThe number that was entered was either too high of a number or typed out, let's try a different question.");
+            }
+
+
+            // ARRAY WITH INTEGERS
+            Console.WriteLine("\n\nSelect a number from 0-4.");
+
+            int[] numbers = { 44, 88, 72, 77, 64 };
+            int guessedNum = Convert.ToInt32(Console.ReadLine());
+
+            if (guessedNum <= 4)
+            {
+                Console.WriteLine(numbers[guessedNum]);
+            }
+            else
+            {
+                Console.WriteLine("\nWow, um you really don't know what I'm asking hugh. how about this one?");
+            }
+
+
+            // STRING LIST
+            Console.WriteLine("\n\nLast one, pick a number from 0 or 1.");
+
+            List<string> zeroOrOne = new List<string>();
+            zeroOrOne.Add("You chose 0 very nice! And that's the end.");
+            zeroOrOne.Add("You chose 1, I would have to. And that's the end");
+
+            int stringGuess = Convert.ToInt32(Console.ReadLine());
+
+            if (stringGuess <= 1)
+            {
+                Console.WriteLine(zeroOrOne[stringGuess]);
+            }    
+            else
+            {
+                Console.WriteLine("\nWell game over if you can even call this a game.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Notes and practice on C#/ArraysAndLists/ArraysAndLists/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            // LIST
+            // LIST CAN USE ANY DATA TYPE
+            List<int> intList = new List<int>();
+
+            intList.Add(4);
+            intList.Add(10);
+            intList.Remove(10);
+
+            Console.WriteLine(intList[0]);
+            Console.ReadLine();
+
+
+        // ARRAY WHICH YOU CAN NOT CHANGE THE SIZE OF THE ARRAY ONCE SET
+
+        // STORING PICTURES
+            //byte[] byteArray = new byte[];
+
+        // SLOW WAY
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 2;
+            //numArray[2] = 10;
+            //numArray[3] = 200;
+            //numArray[4] = 5000;
+
+        // FASTER WAY
+            //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+        // EVEN FASTER WAY
+            //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        // YOU CAN CHANGE A VALUE IN AN ARRAY
+            //numArray2[5] = 650;
+
+            //Console.WriteLine(numArray2[5]);
+            //Console.ReadLine();
+        }
+    }
+}
