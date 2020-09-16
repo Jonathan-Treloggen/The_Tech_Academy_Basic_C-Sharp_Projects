5e1df7d256b85ed88ca7bc0d47398ca4b36305a1
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-15 17:23:31 -0700

    StringAssignment folder added to Assignment folder

---
+++ Basic_C#_Programs/Assignments/StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string name = "Jon";
+            string name2 = "Megan";
+            string name3 = "Megan G.";
+
+            // UPPERCASE
+            name = name.ToUpper();
+
+            // CONCATENATES
+            Console.WriteLine(name + " and " + name2 + ", are friends with " + name3 + ".");
+
+            // STRINGBUILDER
+            StringBuilder sb = new StringBuilder();
+            sb.Append("\nMy name is Jon. ");
+            sb.Append("I've been programming for only three months. ");
+            sb.Append("I'm having a lot of fun coding, figuring out how it all works and actually getting it.");
+            sb.Append("This boot camp, in my opinion was completely worth it.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Notes and practice on C#/String/String/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jon";
+            string quote = "The man said, \"Hello\", Jon. \nHello on a new line. \n\tHello on a tab";
+            string fileName = "C:\\User\\Jon";
+            // OR DO IT THIS WAY WITH AN @ SYMBOL
+            string fileName2 = @"C:\User\Jon";
+
+            // DOES A NAME CONTAIN A SPECIFIC LETTER
+            bool trueOrFalse = name.Contains("n");
+
+            // WHAT IT ENDS WITH
+            trueOrFalse = name.EndsWith("o");
+
+            // HOW LONG A NAME IS
+            int length = name.Length;
+
+            // LOWER CASE STRING
+            name = name.ToLower();
+
+            // UPPER  CASE STRING
+            name = name.ToUpper();
+
+            // IF THIS HAPPENS YOU ARE ADDING CODE NOT REPLACING
+            string name2 = "Jon";
+            name2 = "Megan";
+
+            // PERFERED WAY
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jon");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
