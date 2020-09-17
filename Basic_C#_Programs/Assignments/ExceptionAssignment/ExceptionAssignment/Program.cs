9a077fb8f01fb9ccba83abe4506760ee813d2b92
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-17 13:40:18 -0700

    ExceptionAssignment folder added to Assignment folder

---
+++ Basic_C#_Programs/Assignments/ExceptionAssignment/ExceptionAssignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace ExceptionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            
+            try
+            {
+                List<int> numbers = new List<int>() { 15, 20, 64, 35 };
+                int enteredNumber = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int numberToDivide in numbers)
+                {
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        int total = numbers[i] / enteredNumber;
+                        Console.WriteLine(numbers[i] + " divided by you number " + enteredNumber + " equals " + total);
+                    }
+                    break;
+                }
+            }
+            catch (FormatException Fex)
+            {
+                Console.WriteLine("Please type a whole number like this: 4, not this: four.");
+            }
+            catch (DivideByZeroException Zex)
+            {
+                Console.WriteLine("\nPlease don't divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter a whole number.");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("the program has emerged from the try/catch block");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Notes and practice on C#/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number.");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a smaller second number.");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\nDividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+                Console.ReadLine();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by zero.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
