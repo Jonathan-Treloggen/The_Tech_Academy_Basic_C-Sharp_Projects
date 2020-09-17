e278c875c3ba90ba08398d2aaca538443c276859
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-17 10:14:27 -0700

    6partAssignment folder added to Assignment folder

---
+++ Basic_C#_Programs/Assignments/6partAssignment/6partAssignment/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace _6partAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // PART ONE
+            // ARRAY
+            string[] names = { "Jon", "Megan", "Cody", "Specer" };
+
+            Console.WriteLine("What is you dream job?");
+
+            // PERSON ENTERS NAME
+            string dreamJob = Console.ReadLine();
+
+            // LIST OUT ALL THE NAMES IN ARRAY
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine("\n" + names[i] + "'s a " + dreamJob + ".");
+            }
+
+            Console.ReadLine();
+
+            // PART TWO
+            // WARNING INFINITE LOOP AHEAD
+            //Console.WriteLine("Perpare to close out of the Console, unless you like seing all those numbers fly bye on you screen forever.");
+            //Console.ReadLine();
+
+            //for (int infinite = 0; infinite < 1; infinite--)
+            //{
+            //    Console.WriteLine("value : {0}", infinite);
+            //}
+            //Console.ReadLine();
+
+            // FIXED LOOP + PART 3
+            for (int infiniteFixed = 0; infiniteFixed < 11; infiniteFixed++)
+            {
+                Console.WriteLine(infiniteFixed);
+            }
+            Console.ReadLine();
+
+            for (int j = 0; j <= 10; j++)
+            {
+                Console.WriteLine(j);
+            }
+            Console.ReadLine();
+
+            // PART 4
+            List<string> console = new List<string>() { "Xbox", "Playstation", "Switch", "PC/Mac" };
+            Console.WriteLine("Which console do you prefer " + console[0] + ", " + console[1] + ", " + console[2] + " or " + console[3] + "? Please type is exactly as typed please.");
+            string choice = Console.ReadLine();
+
+            foreach (string name in console)
+            {
+                if (name == choice)
+                {
+                    Console.WriteLine("\nI love playing games on the " + name + " as well!");
+                }
+                else
+                {
+                    Console.WriteLine("What you typed was either not on the list or was not typed correctly");
+                    break;
+                }
+            }
+            Console.ReadLine();
+
+            // PART 5
+            List<string> food = new List<string>() { "PB&J", "Grilled cheese", "Trukey", "PB&J" };
+            Console.WriteLine("Which of these sandwiches do you like the most? " + food[0] + ", " + food[1] + ", " + food[2] + " or " + food[3]);
+            string bestSandwich = Console.ReadLine();
+
+            foreach (string sandwich in food)
+            {
+                if (sandwich == bestSandwich)
+                {
+                    Console.WriteLine("Thats one of my favorite sandwich.");
+                }
+                else
+                {
+                    Console.WriteLine("Come on... please follow the rules.");
+                    break;
+                }
+            }
+            Console.ReadLine();
+
+            // PART 6
+            List<string> favDrinks = new List<string>() { "OJ", "Water", "Milk", "Eggnog", "Water" };
+
+            foreach (string drink in favDrinks)
+            {
+                Console.WriteLine("I love " + drink);
+            }
+            Console.WriteLine("\nAnd yes, Water is on their twice because it's important.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Notes and practice on C#/Iteration/Iteration/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] testScore = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+            //for (int i = 0; i < testScore.Length; i++)
+            //{
+            //    if (testScore[i] > 85)
+            //    {
+            //        Console.WriteLine("Passing test score: " + testScore[i]);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //string[] names = { "Jon", "Megan", "Cody", "Spencer" };
+
+            //for (int j = 0; j < names.Length; j++)
+            //{
+            //    Console.WriteLine(names[j]);
+            //}
+            //Console.ReadLine();
+
+            //List<int> testScore = new List<int>();
+            //testScore.Add(98);
+            //testScore.Add(99);
+            //testScore.Add(81);
+            //testScore.Add(72);
+            //testScore.Add(70);
+
+            //foreach (int score in testScore)
+            //{
+            //    if (score > 85)
+            //    {
+            //        Console.WriteLine("Passing test: " + score);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            //List<string> names = new List<string>() { "Jon", "Megan", "Cody", "Spencer" };
+
+            //foreach (string name in names)
+            //{
+            //    if (name == "Jon")
+            //    {
+            //        Console.WriteLine(name);
+            //    }
+            //}
+            //Console.ReadLine();
+
+            List<int> testScore = new List<int>() { 98, 99, 12, 74, 23, 99 };
+            List<int> passingScores = new List<int>();
+
+            foreach (int score in testScore)
+            {
+                if (score > 85)
+                {
+                    passingScores.Add(score);
+                }
+            }
+            Console.WriteLine(passingScores.Count);
+            Console.ReadLine();
+        }
+    }
+}
