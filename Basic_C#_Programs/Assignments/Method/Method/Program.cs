93a0f1eacf6a8c9b12ba46d6c6d1ba3500615fce
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-21 14:05:43 -0700

    Method folder added to assignments folder

---
+++ Basic_C#_Programs/Assignments/Method/Method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first of two number to be added together.");
+            int numberProblem = mathOperations.addNumbers();
+
+            Console.WriteLine("Both of your numbers added together equals: {0}", numberProblem);
+
+            int numberProblem2 = (int)mathOperations.multiNumbers();
+            Console.WriteLine("Both of your numbers multiplyed together equals: {0}", numberProblem2);
+
+            int numberProblem3 = mathOperations.minusNumber();
+            Console.WriteLine("Both of your numbers subtracted from each other equals: {0}", numberProblem3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Assignments/Method/Method/mathOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class mathOperations
+    {
+        public static int addNumbers()
+        {
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now enter a second number.");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int answer = number1 + number2;
+            return answer;
+        }
+
+        public static decimal multiNumbers()
+        {
+            Console.WriteLine("\nNow its time to multiply two number together. Enter your first one now.");
+            decimal number3 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter your second number.");
+            decimal number4 = Convert.ToDecimal(Console.ReadLine());
+            int answer2 = Convert.ToInt32(number3) * Convert.ToInt32(number4);
+            return answer2;
+        }
+
+        public static int minusNumber()
+        {
+            Console.WriteLine("\nLast but not least put in your first of two numbers to subtract each other.");
+            string number5 = Console.ReadLine();
+            Console.WriteLine("Enter your second number.");
+            string number6 = Console.ReadLine();
+            int answer3 = Convert.ToInt32(number5) - Convert.ToInt32(number6);
+            return answer3;
+        }
+            
+    }
+}
