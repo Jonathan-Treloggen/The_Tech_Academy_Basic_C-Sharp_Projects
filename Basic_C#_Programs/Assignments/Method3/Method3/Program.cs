e1a80ad16b77021a415cb4cb8010fb932f9603b7
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-22 15:30:20 -0700

    Method3 folder added to Assignment folder.

---
+++ Basic_C#_Programs/Assignments/Method3/Method3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number of you choice to be added to 12 and see if you can" +
+                "\nguess the correct number. (hint it's under 100)");
+            int guestNumber = Convert.ToInt32(Console.ReadLine());
+
+            voidMethod.vMethod(guestNumber);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Assignments/Method3/Method3/voidMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3
+{
+    class voidMethod
+    {
+        public static void vMethod(int number1, int number2 = 72)
+        {
+            int mathProb = number1 + 12;
+            Console.WriteLine("{0} is just not as good as {1}", mathProb, number2);
+            Console.ReadLine();
+        }
+    }
+}
