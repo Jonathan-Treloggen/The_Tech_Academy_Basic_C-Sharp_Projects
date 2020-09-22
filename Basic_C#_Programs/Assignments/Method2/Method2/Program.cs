055d2c9aa608a80352df7a1504fea9cc765a923b
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-22 13:34:52 -0700

    posting folder for help.

---
+++ Basic_C#_Programs/Assignments/Method2/Method2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            twoInt optionPer = new twoInt();
+            
+            
+
+            Console.WriteLine("Enter in a number.");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Now enter a second number if you'd like if not hit enter.");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            int answer = twoInt.optionalMethods(firstNum, secondNum);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/Assignments/Method2/Method2/twoInt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class twoInt
+    {
+        public static int optionalMethods(int enteredNumber, int constantNum = 10)
+        {
+            int result = enteredNumber + constantNum;
+            return result;
+        }
+    }
+}
