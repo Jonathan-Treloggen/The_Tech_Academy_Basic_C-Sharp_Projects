1fa1c2edbd5f1deaca0336630d99d77e89a43ded
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-09-29 16:23:08 -0700

    structs folder added to Assignment folder

---
+++ Basic_C#_Programs/Assignments/structs/structs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number;
+            number.Amount = 35.5M;
+
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+        public struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}
