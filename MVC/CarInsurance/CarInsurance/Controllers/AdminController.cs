1aab1cc7a34ae283c2bd6b8d0473ef376a175412
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-10-26 17:05:02 -0700

    Finished CarInsurance if the Assignment folder

---
+++ MVC/CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var signups = db.Insurees;
+                var signupVms = new List<SignupVm>();
+                foreach (var signup in signups)
+                {
+                    var signupVm = new SignupVm();
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVms.Add(signupVm);
+                }
+                return View(signupVms);
+            }
+        }
+    }
+}
--- MVC/CarInsurance/CarInsurance/Controllers/InsureeController.cs
+++ MVC/CarInsurance/CarInsurance/Controllers/InsureeController.cs
@@ -50,6 +50,53 @@ namespace CarInsurance.Controllers
         {
             if (ModelState.IsValid)
             {
+                decimal quoteTotal = 50;
+                var today = DateTime.Today;
+                var age = today.Year - insuree.DateOfBirth.Year;
+                if (age <= 18)
+                {
+                    quoteTotal += 100;
+                }
+                if (age >= 19 && age <= 25)
+                {
+                    quoteTotal += +50;
+                }
+                if (age > 25)
+                {
+                    quoteTotal += +25;
+                }
+                if (insuree.CarYear < 2000)
+                {
+                    quoteTotal += +25;
+                }
+                if (insuree.CarYear > 2015)
+                {
+                    quoteTotal += +25;
+                }
+                if (insuree.CarMake == "Porsche")
+                {
+                    quoteTotal += +25;
+                }
+                if (insuree.CarMake == "Porsche" && insuree.CarModel == "911 Carrera")
+                {
+                    quoteTotal += +25;
+                }
+                if (insuree.SpeedingTickets > 0)
+                {
+                    decimal h = insuree.SpeedingTickets * 10;
+                    quoteTotal += h;
+                }
+                if (insuree.DUI == true)
+                {
+                    quoteTotal *= 1.25m;
+                }
+                if (insuree.CoverageType == true)
+                {
+                    quoteTotal *= 1.5m;
+                }
+
+                insuree.Quote = quoteTotal;
+
                 db.Insurees.Add(insuree);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -82,6 +129,53 @@ namespace CarInsurance.Controllers
         {
             if (ModelState.IsValid)
             {
+                decimal quoteTotal = 50;
+                var today = DateTime.Today;
+                var age = today.Year - insuree.DateOfBirth.Year;
+                if (age <= 18)
+                {
+                    quoteTotal += 100;
+                }
+                if (age >= 19 && age <= 25)
+                {
+                    quoteTotal += +50;
+                }
+                if (age > 25)
+                {
+                    quoteTotal += +25;
+                }
+                if (insuree.CarYear < 2000)
+                {
+                    quoteTotal += +25;
+                }
+                if (insuree.CarYear > 2015)
+                {
+                    quoteTotal += +25;
+                }
+                if (insuree.CarMake == "Porsche")
+                {
+                    quoteTotal += +25;
+                }
+                if (insuree.CarMake == "Porsche" && insuree.CarModel == "911 Carrera")
+                {
+                    quoteTotal += +25;
+                }
+                if (insuree.SpeedingTickets > 0)
+                {
+                    decimal h = insuree.SpeedingTickets * 10;
+                    quoteTotal += h;
+                }
+                if (insuree.DUI == true)
+                {
+                    quoteTotal *= 1.25m;
+                }
+                if (insuree.CoverageType == true)
+                {
+                    quoteTotal *= 1.5m;
+                }
+
+                insuree.Quote = quoteTotal;
+
                 db.Entry(insuree).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
