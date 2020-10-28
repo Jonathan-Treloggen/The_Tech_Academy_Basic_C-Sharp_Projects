9fd59fe3ea843545d07fb81dab7fed5b2944682d
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-10-28 11:40:24 -0700

    CodeFirst folder added

---
+++ Basic_C#_Programs/C-Sharp_Challange/CodeFirst/CodeFirst/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalChallengeCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            using (var db = new BloggingContext())
+            {
+                Console.WriteLine("Please enter the first name of the student you would like to be entered into the database.");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                Console.WriteLine("This is all of the names entered for the student database.");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadLine();
+            }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Name { get; set; }
+
+            public virtual List<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public virtual Blog Blog { get; set; }
+        }
+
+        public class BloggingContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+        }
+    }
+}
