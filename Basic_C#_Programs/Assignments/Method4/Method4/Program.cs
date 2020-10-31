b9a89a1630e06a76d745dcdf568f71e8890ac96b
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-10-31 12:33:38 -0700

    Added assignment requirment to Method4 folder.

--- Basic_C#_Programs/Assignments/Method4/Method4/Program.cs
+++ Basic_C#_Programs/Assignments/Method4/Method4/Program.cs
@@ -6,27 +6,25 @@ using System.Threading.Tasks;
 
 namespace Method4
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
             // ESTABLISHING AN INT FOR MY OUT PARAMETER IN division CLASS
-            int divNumber2;
             int num1;
-            int num2;
 
             Console.WriteLine("Enter a even number to be divided by two.");
             int input = Convert.ToInt32(Console.ReadLine());
 
             division div = new division();
-            div.divM(input, out divNumber2);
+            div.divM(input);
 
-            Console.WriteLine("\nI love dividing numbers by " + divNumber2);
+            Console.WriteLine("\nI love dividing numbers by two.");
 
             // OVERLOADED METHOD BEFORE STATIC WAS ADDED TO CLASS
-            //div.divM(out num1, out num2);
+            //div.divM(out num1);
 
-            division.divM(out num1, out num2);
+            division.divM(out num1);
 
             Console.ReadLine();
         }
--- Basic_C#_Programs/Assignments/Method4/Method4/division.cs
+++ Basic_C#_Programs/Assignments/Method4/Method4/division.cs
@@ -8,18 +8,18 @@ namespace Method4
 {
     class division
     {
-        public void divM(int number1, out int number2)
+        public void divM(int number1)
         {
             // GIVING number2 A VALUE TO BE USED IN THE MAIN PROGRAM
-            number2 = 2;
+            int number2 = 2;
             int number3 = number1 / number2;
             Console.WriteLine(number3);
         }
             
-        public static void  divM( out int number1, out int number2 )
+        public static void  divM(out int number1)
         {
             number1 = 144;
-            number2 = 2;
+            int number2 = 2;
             int number3 = number1 / number2;
             Console.WriteLine("\nI personaly like dividing {0} by {1} because it eaqual my favorite number {2}.", number1, number2, number3);
         }
