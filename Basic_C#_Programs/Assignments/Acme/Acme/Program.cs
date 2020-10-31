a31f8c2ec84a0a6d24b2f3cffaab444797d2be33
Jonathan Treloggen <jonathan.treloggen@gmail.com>
2020-10-31 14:32:13 -0700

    Acme assignment added to Assignments folder

---
+++ Basic_C#_Programs/Assignments/Acme/Acme/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we\'re \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
